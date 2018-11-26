a99871d510855530fc800fdd4d03935ba2100569
MelikaH <melikahodzic@hotmail.com>
2018-11-26 10:18:02 +0100

    poslije Erminovog pulla

---
+++ resources/views/includes/foot.blade.php
@@ -0,0 +1,65 @@
+<br>
+<hr>
+<br>
+<div class="container text-center text-md-left mt-5">
+
+      <!-- Grid row -->
+      <div class="row mt-3">
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+          <!-- Content -->
+          <h6 class="text-uppercase font-weight-bold">Company name</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>Air Quality Website is specialized tool for tracking and displaying the PM2.5 particles in air of the capital of BiH, Sarajevo, in order to present this serious problem to our citizents and let them know how to act according to specific case.</p>
+
+        </div>
+        
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+          <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">Useful links</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <a href="https://ba.usembassy.gov/bs/embassy-bs/sarajevo-bs/air-quality-monitor-sarajevo-bs/"> US Embassy Air Quality Track</a>
+          </p>
+          
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+          <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">Contact</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <i class="fa fa-home mr-3"></i> Ulica Roberta C. Frasurea 1 71000 Sarajevo</p>
+          <p>
+            <i class="fa fa-envelope mr-3"></i> info@example.com</p>
+          <p>
+            <i class="fa fa-phone mr-3"></i> +387 33 704-000</p>
+          <p>
+            <i class="fa fa-print mr-3"></i> +387 33 659-722</p>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3" style="background:#282828">© 2018 Copyright:
+      <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+
+  
